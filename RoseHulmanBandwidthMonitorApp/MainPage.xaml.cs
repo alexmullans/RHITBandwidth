4558b6a0df7f3ee5fabcee758bfd0e5f8faa06de
Alex Mullans <alex@alexmullans.com>
2012-11-28 00:37:33 -0500

    Add result caching and progress bar for network.

--- RoseHulmanBandwidthMonitorApp/MainPage.xaml.cs
+++ RoseHulmanBandwidthMonitorApp/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -17,41 +18,69 @@ namespace RoseHulmanBandwidthMonitorApp
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
+        public ImageBrush PanoramaBackgroundImage
+        {
+            get
+            {
+                var lightThemeEnabled = (Visibility)Application.Current.Resources["PhoneLightThemeVisibility"] ==
+                                        Visibility.Visible;
+                var url = lightThemeEnabled ? "/Assets/background_light.jpg" : "/Assets/background.jpg";
+                return new ImageBrush() { ImageSource = new BitmapImage(new Uri(url, UriKind.Relative)) };
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
-            Scraper.Scrape(this);
+            Loaded += MainPageLoaded;
         }
 
-        public void UpdateUi(BandwidthResults bandwidthResults)
+        private void MainPageLoaded(object sender, RoutedEventArgs e)
         {
-            Deployment.Current.Dispatcher.BeginInvoke(() =>
-                                                          {
-                                                              foreach (
-                                                                  var control in
-                                                                      new Dictionary<BandwidthMeter, String>()
-                                                                      {{PolicyDown, bandwidthResults.PolicyReceived}, 
-                                                                      {PolicyUp, bandwidthResults.PolicySent},
-                                                                      {ActualDown, bandwidthResults.ActualReceived},
-                                                                      {ActualUp, bandwidthResults.ActualSent}})
-                                                              {
-                                                                  control.Key.UpdateBorder(GetBandwidthNumberFromString(
-                                                                                   control.Value), PolicyUsageGrid.ActualHeight);
-                                                                  control.Key.UsageTextBlock.Text =
-                                                                      control.Value;
-                                                              }
-                                                          });
+            if (IsolatedStorageSettings.ApplicationSettings.Contains("BandwidthClass"))
+                UpdateUi(BandwidthResults.RetrieveFromIsolatedStorage(), false);
 
+            var indicator = new ProgressIndicator();
+            indicator.IsVisible = true;
+            indicator.Text = "Updating usage...";
+            indicator.IsIndeterminate = true;
+            SystemTray.ProgressIndicator = indicator;
+
+            new Thread(Scraper.Scrape).Start(this);
+        }
+
+        private void StopProgressIndicator()
+        {
+            SystemTray.ProgressIndicator.IsVisible = false;
+        }
+
+        public void UpdateUi(BandwidthResults bandwidthResults, bool fromNetwork)
+        {
+            foreach (var control in
+                       new Dictionary<BandwidthMeter, String>()
+                                                                          {{PolicyDown,bandwidthResults.PolicyReceived},
+                                                                              {PolicyUp, bandwidthResults.PolicySent},
+                                                                              {ActualDown,bandwidthResults.ActualReceived},
+                                                                              {ActualUp, bandwidthResults.ActualSent}})
+            {
+                control.Key.UpdateBorder(GetBandwidthNumberFromString(
+                    control.Value), PolicyUsageGrid.ActualHeight);
+                control.Key.UsageTextBlock.Text =
+                    control.Value;
+            }
 
             var tileData = new FlipTileData()
-            {
-                BackContent = GetBandwidthStringForTile(bandwidthResults),
-                Title = "Bandwidth Monitor",
-                Count = Convert.ToInt32(GetBandwidthNumberFromString(bandwidthResults.PolicyReceived)/1000)
-            };
+                               {
+                                   BackContent = GetBandwidthStringForTile(bandwidthResults),
+                                   Title = "Bandwidth Monitor",
+                                   Count =
+                                       Convert.ToInt32(GetBandwidthNumberFromString(bandwidthResults.PolicyReceived) /
+                                                       1000)
+                               };
             var primaryTile = ShellTile.ActiveTiles.First();
             primaryTile.Update(tileData);
+            if (fromNetwork)
+                StopProgressIndicator();
         }
 
         private static String GetBandwidthStringForTile(BandwidthResults results)
@@ -72,17 +101,6 @@ namespace RoseHulmanBandwidthMonitorApp
             MessageBox.Show(
                 "The credentials you entered don't seem to be working, or we can't find the bandwidth tool right now.");
             NavigationService.Navigate(new Uri("/SettingsPage.xaml", UriKind.Relative));
-        }
-
-        public ImageBrush PanoramaBackgroundImage
-        {
-            get
-            {
-                var lightThemeEnabled = (Visibility)Application.Current.Resources["PhoneLightThemeVisibility"] ==
-                                        Visibility.Visible;
-                var url = lightThemeEnabled ? "/Assets/background_light.jpg" : "/Assets/background.jpg";
-                return new ImageBrush() { ImageSource = new BitmapImage(new Uri(url, UriKind.Relative)) };
-            }
         }
     }
 }
--- RoseHulmanBandwidthMonitorApp/Scraper.cs
+++ RoseHulmanBandwidthMonitorApp/Scraper.cs
@@ -22,6 +22,26 @@ namespace RoseHulmanBandwidthMonitorApp
         public String PolicySent { get; internal set; }
         public String ActualReceived { get; internal set; }
         public String ActualSent { get; internal set; }
+        public void SaveToIsolatedStorage()
+        {
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+            settings["BandwidthClass"] = BandwidthClass;
+            settings["PolicyRecieved"] = PolicyReceived;
+            settings["PolicySent"] = PolicySent;
+            settings["ActualReceived"] = ActualReceived;
+            settings["ActualSent"] = ActualSent;
+        }
+        public static BandwidthResults RetrieveFromIsolatedStorage()
+        {
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+            var toReturn = new BandwidthResults();
+            toReturn.BandwidthClass = (String)settings["BandwidthClass"];
+            toReturn.PolicyReceived = (String)settings["PolicyRecieved"];
+            toReturn.PolicySent = (String)settings["PolicySent"];
+            toReturn.ActualReceived = (String)settings["ActualReceived"];
+            toReturn.ActualSent = (String)settings["ActualSent"];
+            return toReturn;
+        }
     }
 
     public class Scraper
@@ -29,15 +49,15 @@ namespace RoseHulmanBandwidthMonitorApp
         private static MainPage _page;
         private const int BUFFER_SIZE = 1024;
 
-        public static void Scrape(MainPage page)
+        public static void Scrape(object page)
         {
-            _page = page;
+            _page = (MainPage)page;
 
             var web = new HtmlWeb();
             web.LoadCompleted += ParseBandwidthDocument;
             var settings = IsolatedStorageSettings.ApplicationSettings;
             web.LoadAsync("http://netreg.rose-hulman.edu/tools/networkUsage.pl",
-                new UTF8Encoding(), 
+                new UTF8Encoding(),
                 (String)settings["user"],
                 (String)settings["pass"],
                 "rose-hulman");
@@ -58,16 +78,16 @@ namespace RoseHulmanBandwidthMonitorApp
                                select desc.ParentNode.ParentNode;
 
             var resultsList = summaryTable.ElementAt(0).Elements("tr").ElementAt(1).Elements("td");
-
-            _page.UpdateUi(new BandwidthResults()
-                                  {
-                                      BandwidthClass = resultsList.ElementAt(0).InnerText,
-                                      PolicyReceived = resultsList.ElementAt(1).InnerText,
-                                      PolicySent = resultsList.ElementAt(2).InnerText,
-                                      ActualReceived = resultsList.ElementAt(3).InnerText,
-                                      ActualSent = resultsList.ElementAt(4).InnerText
-                                  });
-
+            var results = new BandwidthResults()
+                              {
+                                  BandwidthClass = resultsList.ElementAt(0).InnerText,
+                                  PolicyReceived = resultsList.ElementAt(1).InnerText,
+                                  PolicySent = resultsList.ElementAt(2).InnerText,
+                                  ActualReceived = resultsList.ElementAt(3).InnerText,
+                                  ActualSent = resultsList.ElementAt(4).InnerText
+                              };
+            Deployment.Current.Dispatcher.BeginInvoke(() => _page.UpdateUi(results, true));
+            results.SaveToIsolatedStorage();
         }
     }
 }
